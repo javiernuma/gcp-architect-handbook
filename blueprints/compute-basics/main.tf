46167478582f8b3806885295f8a77ea8fb552bc6
javiernuma <ing.javiernuma@gmail.com>
2026-01-03 00:37:01 -0500

    fix: correct variable block syntax by removing commas

--- blueprints/compute-basics/main.tf
+++ blueprints/compute-basics/main.tf
@@ -9,10 +9,10 @@ module "virtualmachine" {
   image_family  = var.image_family
 
   # Red y Seguridad
-  network_name  = var.network_name
-  subnet_name   = var.subnet_name
-  is_public     = var.is_public
-  network_tags  = var.network_tags # Esto habilita el Firewall para el puerto 80
+  fix: correct variable block syntax by removing commasfix: correct variable block syntax by removing commas  network_name = var.network_name
+  subnet_name  = var.subnet_name
+  is_public    = var.is_public
+  network_tags = var.network_tags # Esto habilita el Firewall para el puerto 80
 
   # Automatización de NGINX
   # Leemos el script de instalación que creaste en la carpeta del módulo
--- modules/compute/virtual-machine/variables.tf
+++ modules/compute/virtual-machine/variables.tf
@@ -1,10 +1,42 @@
-variable "project_id" { type = string }
-variable "instance_name" { type = string }
-variable "machine_type" { type = string, default = "e2-medium" }
-variable "zone" { type = string }
-variable "image_family" { type = string, default = "debian-cloud/debian-11" }
-variable "network_name" { type = string, default = "default" }
-variable "subnet_name" { type = string, default = "default" }
-variable "is_public" { type = bool, default = false }
-variable "network_tags" { type = list(string), default = [] }
-variable "startup_script" { type = string, default = "" }
+variable "instance_name" {
+  type = string
+}
+
+variable "machine_type" {
+  type    = string
+  default = "e2-medium"
+}
+
+variable "zone" {
+  type = string
+}
+
+variable "image_family" {
+  type    = string
+  default = "debian-cloud/debian-11"
+}
+
+variable "network_name" {
+  type    = string
+  default = "default"
+}
+
+variable "subnet_name" {
+  type    = string
+  default = "default"
+}
+
+variable "is_public" {
+  type    = bool
+  default = false
+}
+
+variable "network_tags" {
+  type    = list(string)
+  default = []
+}
+
+variable "startup_script" {
+  type    = string
+  default = ""
+}
