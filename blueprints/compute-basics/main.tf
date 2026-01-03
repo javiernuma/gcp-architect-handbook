7b3959180d6d3d13f6437834d02de1d894dbfdf4
javiernuma <ing.javiernuma@gmail.com>
2026-01-02 22:34:47 -0500

    chore: add professional .gitignore and fix terraform formatting

--- blueprints/compute-basics/main.tf
+++ blueprints/compute-basics/main.tf
@@ -1,9 +1,10 @@
-# blueprints/lab-01-compute-basics/main.tf
-module "my_lab_vm" {
-  source        = "../../modules/compute/virtual-machine"
+module "lab_vm" {
+  source        = "../../modules/compute/virtual-machine" # Ruta relativa al módulo
+  project_id    = var.project_id
   instance_name = "instance-lab-1"
   machine_type  = "e2-medium"
+  zone          = "us-central1-a"
   network_tags  = ["http-server"]
-  is_public     = true  # Lo que pide el lab 484532
-  startup_script = "apt-get update && apt-get install -y apache2"
+  is_public     = true # El lab pide que sea accesible por IP externa
+  image_family  = "debian-cloud/debian-11"
 }
