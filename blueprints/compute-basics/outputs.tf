a8e1836c34e910a5426653631187ba7c92a2d9b0
javiernuma <ing.javiernuma@gmail.com>
2026-01-03 00:15:17 -0500

    fix: align module naming in outputs

--- blueprints/compute-basics/outputs.tf
+++ blueprints/compute-basics/outputs.tf
@@ -1,9 +1,3 @@
-output "external_ip" {
-  # Asegúrate de que el nombre después del punto coincida con el nombre en tu main.tf
-  value       = google_compute_instance.vm_instance.network_interface[0].access_config[0].nat_ip
-  description = "IP pública de la instancia"
-}
-
 output "instance_name" {
   value = google_compute_instance.vm_instance.name
 }
---
+++ modules/compute/virtual-machine/outputs.tf
@@ -0,0 +1,5 @@
+output "external_ip" {
+  # Asegúrate de que el nombre después del punto coincida con el nombre en tu main.tf
+  value       = google_compute_instance.vm_instance.network_interface[0].access_config[0].nat_ip
+  description = "IP pública de la instancia"
+}
